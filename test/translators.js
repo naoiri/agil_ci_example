f6740e0f5c4b78600dc19ef365bd75d6217428e0
Naoya Irikura <naoiri2001@skola.goteborg.se>
2021-03-03 16:26:42 +0100

    Added test for Allsprak

--- test/translators.js
+++ test/translators.js
@@ -1,26 +1,35 @@
 import assert from "assert"
-import { toRobber, toNormal } from "./../src/translators.js"
+import { toRobber, toNormal, toAllsprak } from "./../src/translators.js"
 
-describe("Translators", function () {
-    describe("#toRobber", function () {
-        it("should work for empty string", function () {
+describe("Translators", function() {
+
+    describe("#toRobber", function() {
+        it("should work for empty string", function() {
             assert.strictEqual(toRobber(""), "")
         })
-        it("should work for simple input", function () {
+        it("should work for simple input", function() {
             assert.strictEqual(toRobber("Hejsan hoppsan!"), "Hohejojsosanon hohopoppopsosanon!")
         })
     })
-    describe("#toNormal", function () {
-        it("should work for empty string", function () {
+    describe("#toNormal", function() {
+        it("should work for empty string", function() {
             assert.strictEqual(toNormal(""), "")
         })
-        it("should work for regular input", function () {
+        it("should work for regular input", function() {
             const input = "Hejsan hoppsan!"
             assert.strictEqual(toNormal(input), input)
         })
-        it("should work for robber language", function () {
+        it("should work for robber language", function() {
             const input = "Hejsan hoppsan!"
             assert.strictEqual(toNormal(toRobber(input)), input)
+        })
+    })
+    describe("#toAllsprak", function() {
+        it("should work for empty string", function() {
+            assert.strictEqual(toAllsprak(""), "")
+        })
+        it("should work for simple input", function() {
+            assert.strictEqual(toAllsprak("Hejsan"), "Hallejsan")
         })
     })
 })
